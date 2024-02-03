38078f1a14ca8a3bd23c5de3df1273e88fd7d495
ilenkovalv <ilenkovalv@lad24.ru>
2024-02-03 20:02:07 +0300

    increment 1

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,3 +1,91 @@
 package main
 
-func main() {}
+import (
+	"io"
+	"math/rand"
+	"net/http"
+)
+
+type UrlItem struct {
+	id      string
+	longUrl string
+}
+
+var urlData []UrlItem
+
+func GetItemById(data []UrlItem, id string) (item UrlItem, isFind bool) {
+	for _, item := range data {
+		if item.id == id {
+			return item, true
+		}
+	}
+	return UrlItem{}, false
+}
+
+func GenerateToken(length int) string {
+	letterBytes := "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890"
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = letterBytes[rand.Intn(len(letterBytes))]
+	}
+	return string(b)
+}
+func Handler(w http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case http.MethodPost:
+		PostUrl(w, r)
+	case http.MethodGet:
+		GetUrlById(w, r)
+	default:
+		http.Error(w, "Неверный метод", http.StatusBadRequest)
+		return
+	}
+}
+
+func PostUrl(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("Content-Type") != "text/plain" {
+		http.Error(w, "", http.StatusUnsupportedMediaType)
+		return
+	}
+	responseData, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Не удалось прочитать тело запроса", http.StatusBadRequest)
+		return
+	}
+	responseString := string(responseData)
+	//генерируем уникальный токен
+	token := GenerateToken(8)
+	// записываем в структуру
+	item := UrlItem{id: token, longUrl: responseString}
+	urlData = append(urlData, item)
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusCreated)
+	io.WriteString(w, "http://localhost:8080/"+token)
+
+}
+
+func GetUrlById(w http.ResponseWriter, r *http.Request) {
+
+	id := r.URL.String()[1:]
+	url, isFind := GetItemById(urlData, id)
+	if !isFind {
+		http.Error(w, "Ссылка по ID не найдена", http.StatusBadRequest)
+		return
+	}
+	w.Header().Set("Location", url.longUrl)
+	w.WriteHeader(http.StatusTemporaryRedirect)
+
+}
+
+func main() {
+
+	mux := http.NewServeMux()
+	mux.HandleFunc(`/`, Handler)
+
+	err := http.ListenAndServe(`:8080`, mux)
+	if err != nil {
+		panic(err)
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/nuvotlyuba/Go-yandex
+
+go 1.21.0
