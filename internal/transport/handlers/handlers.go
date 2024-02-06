103ba1f6cf083508d0ca685c674a9790cee4f299
ilenkovalv <ilenkovalv@lad24.ru>
2024-02-06 22:13:37 +0300

    delete fmt

--- internal/transport/handlers/handlers.go
+++ internal/transport/handlers/handlers.go
@@ -2,7 +2,6 @@ package handlers
 
 import (
 	"flag"
-	"fmt"
 	"io"
 	"net/http"
 	"strings"
@@ -32,7 +31,6 @@ func BasicRouter() chi.Router {
 
 func PostUrlHandler(w http.ResponseWriter, r *http.Request) {
 	flag.Parse()
-	fmt.Println(baseUrl, "handler")
 
 	contentType := r.Header.Get("Content-Type")
 	if !strings.Contains(contentType, "text/plain") {
