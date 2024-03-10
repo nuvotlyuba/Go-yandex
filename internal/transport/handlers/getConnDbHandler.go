b4c9ff7059d3afdd09d2718d414799efce7827a1
ilenkovalv <ilenkovalv@lad24.ru>
2024-03-10 17:46:30 +0300

    fix static test

--- internal/transport/handlers/getConnDbHandler.go
+++ internal/transport/handlers/getConnDbHandler.go
@@ -7,13 +7,13 @@ import (
 	"github.com/nuvotlyuba/Go-yandex/internal/services"
 )
 
-func (h Handlers) GetConnDBHandler(w http.ResponseWriter, _ *http.Request) {
+func (h Handlers) GetConnDBHandler(w http.ResponseWriter, r *http.Request) {
 	//пингануть базу
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 	s := new(services.Service)
 	if err := s.PingDB(ctx);err != nil {
-		http.Error(w, error.Error(err), http.StatusBadRequest)
+		http.Error(w, error.Error(err), http.StatusInternalServerError)
 		return
 	}
 
